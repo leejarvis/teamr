f35acf2bb1e2b014e661988c8a7a708e99020815
Lee Jarvis <ljjarvis@gmail.com>
2012-12-18 19:20:05 +0000

    extract(epoch from last_assigned_contact_at)

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < ActiveRecord::Base
   has_and_belongs_to_many :assigned_users, class_name: 'User'
 
   def auto_assign_team_member(project)
-    team = project.teams(true).order('last_assigned_contact_at ASC').first
+    team = project.teams(true).order('extract(epoch from last_assigned_contact_at) ASC').first
     if team && (user = team.user)
       self.assigned_users << user
       team.send(:write_attribute, :last_assigned_contact_at, Time.current)
